5f6ae2e885116061b60937190d31b1c4bba76737
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-07-21 18:43:07 +0100

    edit AppInitializer

--- src/Shared/Eshrimp.Shared.Infrastructure/Services/AppInitializer.cs
+++ src/Shared/Eshrimp.Shared.Infrastructure/Services/AppInitializer.cs
@@ -1,6 +1,17 @@
-﻿namespace Eshrimp.Shared.Infrastructure.Services
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Eshrimp.Shared.Infrastructure.Services
 {
-    public class AppInitializer
+    public class AppInitializer : IHostedService
     {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
