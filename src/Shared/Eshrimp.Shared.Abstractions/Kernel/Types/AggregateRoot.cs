2c249a58cfdcb3236777f7d46155b349adb9e4f9
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-07-23 20:20:41 +0100

    add TypeId, AggregateId, AggregateRoot, IDomainEvent

---
+++ src/Shared/Eshrimp.Shared.Abstractions/Kernel/Types/AggregateRoot.cs
@@ -0,0 +1,42 @@
+﻿namespace Eshrimp.Shared.Abstractions.Kernel.Types
+{
+    public class AggregateRoot<T>
+    {
+        public T Id { get; private set; }
+        public int Version { get; private set; }
+        public IEnumerable<IDomainEvent> Events => _domainEvents;
+
+        private readonly List<IDomainEvent> _domainEvents = new();
+        private bool _versionIncremented;
+        
+        public void AddEvent(IDomainEvent domainEvent)
+        {
+            if (!_versionIncremented && !_domainEvents.Any())
+            {
+                Version++;
+            }
+
+            _domainEvents.Add(domainEvent);
+            _versionIncremented = true;
+
+        }
+
+        protected void IncrementVersion()
+        {
+            if (_versionIncremented) 
+            {
+                return;
+            }
+
+            Version++;
+            _versionIncremented = true;
+        }
+
+        public void ClearEvents() => _domainEvents.Clear();
+    }
+
+    public abstract class AggregateRoot : AggregateRoot<AggregateId>
+    {
+
+    }
+}
