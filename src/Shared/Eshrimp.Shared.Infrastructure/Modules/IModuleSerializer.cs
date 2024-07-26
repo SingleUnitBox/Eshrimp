fb661331664ca779aa890b5a44ca1648e0282c7f
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-07-26 20:46:31 +0100

    add JsonModuleSerializer, IModuleSerializer, ModuleClient, edit Extensions

---
+++ src/Shared/Eshrimp.Shared.Infrastructure/Modules/IModuleSerializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Eshrimp.Shared.Infrastructure.Modules
+{
+    public interface IModuleSerializer
+    {
+        byte[] Serialize(object message);
+        object Deserialize(byte[] message, Type receiverType);
+        T Deserialize<T>(byte[] message);
+    }
+}
--- src/Shared/Eshrimp.Shared.Infrastructure/Modules/ModuleClient.cs
+++ src/Shared/Eshrimp.Shared.Infrastructure/Modules/ModuleClient.cs
@@ -5,24 +5,29 @@ namespace Eshrimp.Shared.Infrastructure.Modules
     public class ModuleClient : IModuleClient
     {
         private readonly IModuleRegistry _moduleRegistry;
+        private readonly IModuleSerializer _moduleSerializer;
 
-        public ModuleClient(IModuleRegistry moduleRegistry)
+        public ModuleClient(IModuleRegistry moduleRegistry,
+            IModuleSerializer moduleSerializer)
         {
             _moduleRegistry = moduleRegistry;
+            _moduleSerializer = moduleSerializer;
         }
 
         public async Task PublishAsync(object message)
         {
-            if (message == null)
-            {
-                return;
-            }
-
-            var registrations = _moduleRegistry.GetBroadcastRegistrations(message.GetType().Name);
+            var key = message.GetType().Name;
+            var registrations = _moduleRegistry.GetBroadcastRegistrations(key);
             foreach (var reg in registrations)
             {
+                var translatedMessage = TranslateType(message, reg.ReceiverType);
                 var action = reg.Action;
+
+                await action(translatedMessage);
             }
         }
+
+        private object TranslateType(object message, Type receiverType)
+            => _moduleSerializer.Deserialize(_moduleSerializer.Serialize(message), receiverType);
     }
 }
